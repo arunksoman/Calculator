05599550465c076fdb91e35adb157605dcdd606a
arunksoman <arunksoman69@gmail.com>
2020-09-27 12:18:31 -0700

    added custom numerical input

--- calc2.py
+++ calc2.py
@@ -1,30 +1,34 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
 from kivy.event import EventDispatcher
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.animation import Animation
 import decimal as d
 import math
+import re
 
 
-class MyEventDispatcher(EventDispatcher):
+class KeypressListener(EventDispatcher):
     def __init__(self, **kwargs):
         self.register_event_type('on_test')
-        super(MyEventDispatcher, self).__init__(**kwargs)
-    def do_something(self, value):
+        super(KeypressListener, self).__init__(**kwargs)
+    def calculation_from_keyboard(self, value):
         app = App.get_running_app()
-        final_result = str(HomeScreen().evaluate(str(app.sm.get_screen('homescreen').ids.input.text)))
+        print("value: ", app.sm.get_screen('homescreen').ids.input.text)
+        final_result = str(HomeScreen().evaluate(str(value)))
         print(f"Final Result: {final_result}")
-        app.sm.get_screen('homescreen').ids.result.text = final_result
         app.sm.get_screen('homescreen').ids.input.focus=True
+        app.sm.get_screen('homescreen').ids.result.text = final_result
         self.dispatch('on_test', value)
     def on_test(self, *args):
-        print ("I am dispatched", args)
+        pass
 
 def my_callback(value, *args):
-    print("Hello, I got an event!", args)
+    # print("Hello, I got an event!", args)
+    pass
 
-def substract(num1=1.2, num2=1):
+def substract(num1, num2):
     if num2 is not isinstance(num2, float):
         num2 = float(num2)
     len_num2 = len(str(num2).split('.')[1])
@@ -39,18 +43,28 @@ def operand_checker(validation_string, operand):
         return True
     return False
 
-class HomeScreen(Screen):
-    def validate(self):
-        operands = ['+', '-', '/', '*', '%']
-        for_validation = self.ids.input.text
-        print(for_validation)
-        check_foroperand = [operand_checker(for_validation, operand) for operand in operands]
-        print(check_foroperand)
-        if for_validation != "":
-            ev = MyEventDispatcher()
-            ev.bind(on_test=my_callback)
-            ev.do_something('test')
 
+class NumberInput(TextInput):
+        r = re.compile(r'^[-+]*?\d*[\*,\-,/,., +]?\d*$')
+        def insert_text(self, substring, from_undo=False):
+            r = self.r
+            app = App.get_running_app()
+            cursor_col = app.sm.get_screen('homescreen').ids.input.cursor_col
+            if r.match(substring):
+                print("Substring: ", substring)
+                print(cursor_col)
+                print("String: ", substring)
+                s = substring
+                app.sm.get_screen('homescreen').ids.warning.text = ""
+                ev = KeypressListener()
+                ev.bind(on_test=my_callback)
+                ev.calculation_from_keyboard('test')
+                
+            else:
+                app.sm.get_screen('homescreen').ids.warning.text = "[b][color=ff0000]Please Enter Valid number[/color][/b]"
+                s = substring.replace(substring, "")
+            return super(NumberInput, self).insert_text(s, from_undo=from_undo)
+class HomeScreen(Screen):
     def backspace(self, cursor):
         print(cursor)
         if cursor != (0, 0):
@@ -74,7 +88,9 @@ class HomeScreen(Screen):
             return ""
         if calc_operation.endswith(('+', '-', '*', '/', '%')):
             return calc_operation[:-1]
-        if not calc_operation.find("-") != -1:
+
+        #Fixed number cannot start with - symbol
+        if not calc_operation.find("-") != -1 or calc_operation.startswith('-'):
             try:
                 result = eval(calc_operation)
                 return result
@@ -94,5 +110,5 @@ class CalculatorApp(App):
 
 
 if __name__ == "__main__":
-    substract()
+    Window.size = (440, 600)
     CalculatorApp().run()
