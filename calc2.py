744e156fcd6cfbffc819d76ccbe53f3a2fa9b829
arunksoman <arunksoman69@gmail.com>
2020-10-04 10:06:55 -0700

    Only to fix IEEE 754 Floating point issue

--- calc2.py
+++ calc2.py
@@ -6,7 +6,7 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 import decimal as d
 import re
-import calculation
+# import calculation
 
 def show_keyboard(event):
     app = App.get_running_app()
@@ -14,9 +14,9 @@ def show_keyboard(event):
 
     
 def operand_checker(validation_string):
-    print(f"Validation String {validation_string}")
+    # print(f"Validation String {validation_string}")
     operand_list = ['+', '-', '*', '/', '%']
-    print("Hai")
+    # print("Hai")
     return [False if validation_string.find(operand) == -1 else True for operand in operand_list]
 
 
@@ -27,9 +27,6 @@ class NumberInput(TextInput):
             app = App.get_running_app()
             cursor_col = app.sm.get_screen('homescreen').ids.input.cursor_col
             if r.match(substring):
-                print("Substring: ", substring)
-                print(cursor_col)
-                print("String: ", substring)
                 s = substring
                 app.sm.get_screen('homescreen').ids.warning.text = ""
             else:
@@ -43,21 +40,16 @@ class HomeScreen(Screen):
             self.ids.warning.text = "[b][color=ff0000]Bracket count mismatching[/color][/b]"
         else:
             self.ids.warning.text = ""
-        # print("Hai")
-        print("value: ", self.ids.input.text)
         final_result = str(HomeScreen().evaluate(str(self.ids.input.text)))
-        print(f"Final Result: {final_result}")
         self.ids.input.focus=True
         self.ids.result.text = final_result
 
     def clear_result(self):
         self.ids.result.text = ""
     def backspace(self, cursor):
-        print(cursor)
         if cursor != (0, 0):
             calculation = self.ids.input.text
             calculation = calculation.replace(calculation[cursor[0] - 1], "")
-            print(calculation)
             self.ids.input.text = calculation
             final_result = str(HomeScreen().evaluate(str(self.ids.input.text)))
             self.ids.result.text = final_result
@@ -73,51 +65,85 @@ class HomeScreen(Screen):
     def evaluate(self, calc_operation):
         if calc_operation == "":
             return ""
-            # return calc_operation[:-1]
         if calc_operation.endswith(('+', '-', '*', '/', '%', '(')):
             return calc_operation[:-1]
-        # try:
-        # result = eval(calc_operation)
-        # splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', calc_operation)))
-        # if operand_checker(calc_operation).count(True) > 1:
-        #     return "To Do"
-        bracket_matching = re.findall("[^()]+", calc_operation)
-        print(bracket_matching)
-        operation_list = []
-        for operation in bracket_matching:
-            if not (operation.endswith(('+', '-', '*', '/', '%')) or operation.startswith(('+', '-', '*', '/', '%'))):
-                if operand_checker(operation).count(True) == 1 or operand_checker(operation).count(False) == 5:
-                    operation = eval(operation)
-                else:
-                    splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', operation)))
-                    operation = ""
-                    result = ""
-                    for index, item in enumerate(splitted_operation):
-                        print(f"item: {item}")
-                        if index < 3:
-                            operation += item
-                        if index >= 3:
-                            print("I am in else")
-                            operation += item
-                            # item = str(eval(operation))
-                        if index % 2 == 0 and not operation.endswith(('+', '-', '*', '/', '%')):
-                            result = str(eval(operation))
-                            print(40*"*")
-                            operation =  result
+        try:
+            if operand_checker(calc_operation)[3]:
+                print("Operation has division")
+                splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', calc_operation)))
+                print("Splitted Operation: ", splitted_operation)
+                for index, item in enumerate(splitted_operation):
+                    if item == '/':
+                        print(splitted_operation[index + 1])
+                        if splitted_operation[index + 1] == "0":
+                            # self.ids.warning.text = "[b][color=ff0000]Cannot Divide by Zero[/color][/b]"
+                            return "[b][color=ff0000]Cannot Divided by Zero.[/b][/color]"
+                        splitted_operation[index - 1] = "({}".format(splitted_operation[index-1])
+                        splitted_operation[index + 1] = "{})".format(splitted_operation[index+1])
+                calc_operation = "".join(splitted_operation)
+                # print("Clac Operation with Brackets: ", calc_operation)
+            bracket_matching = re.findall("[^()]+", calc_operation)
+            print(bracket_matching)
+            operation_list = []
+            for operation in bracket_matching:
+                if not (operation.endswith(('+', '-', '*', '/', '%')) or operation.startswith(('+', '-', '*', '/', '%'))):
+                    if operand_checker(operation).count(True) == 1 or operand_checker(operation).count(False) == 5:
+                        operation = eval(operation)
+                    else:
+                        splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', operation)))
+                        operation = ""
+                        result = ""
+                        for index, item in enumerate(splitted_operation):
+                            print(f"item: {item}")
+                            if index < 3:
+                                operation += item
+                            if index >= 3:
+                                # print("I am in else")
+                                operation += item
+                                # item = str(eval(operation))
+                            if index % 2 == 0 and not operation.endswith(('+', '-', '*', '/', '%')):
+                                # print(f"Operation: {operation}")
+                                result = str(eval(operation))
+                                print(40*"*")
+                                operation =  result
+                            print(f"result: {result}")
+                            print(f"index: {index}")
+                            print(f"operation: {operation}")
                         print(f"result: {result}")
-                        print(f"index: {index}")
-                        print(f"operation: {operation}")
-                    return result
-            operation_list.append(operation)
-        new_operation_str = ''.join([str(operation) for operation in operation_list])
-        splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', new_operation_str)))
-        print(operand_checker(new_operation_str))
-        if operand_checker(new_operation_str).count(False) == 5:
-            return new_operation_str
-        if operand_checker(new_operation_str).count(True) == 1:
-            return eval(new_operation_str)
-        # except Exception:
-        #     return "Error"
+                        return result
+                operation_list.append(operation)
+            new_operation_str = ''.join([str(operation) for operation in operation_list])
+            print(f"New Operation: {new_operation_str}")
+            splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', new_operation_str)))
+            # print(operand_checker(new_operation_str))
+            if operand_checker(new_operation_str).count(True) == 1:
+                result = eval(new_operation_str)
+                print(f"Result1: {result}")
+                return result
+            else:
+                splitted_operation = list(filter(lambda x: x, re.split(r'([-+*/%()])|\s+', new_operation_str)))
+                operation = ""
+                result = ""
+                for index, item in enumerate(splitted_operation):
+                    print(f"item: {item}")
+                    if index < 3:
+                        operation += item
+                    if index >= 3:
+                        # print("I am in else")
+                        operation += item
+                        # item = str(eval(operation))
+                    if index % 2 == 0 and not operation.endswith(('+', '-', '*', '/', '%')):
+                        # print(f"Operation: {operation}")
+                        result = str(eval(operation))
+                        print(40*"*")
+                        operation =  result
+                    print(f"result: {result}")
+                    print(f"index: {index}")
+                    print(f"operation: {operation}")
+                print(f"result: {result}")
+                return str(eval(str(operation)))
+        except Exception:
+            return "Error"
             
 class CalculatorApp(App):
     sm = ScreenManager()
